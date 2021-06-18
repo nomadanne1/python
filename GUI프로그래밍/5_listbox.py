f9ad4a4579164a1c4f5c77a186c9d0be9d90cf3c
CookieYi <nomadanne1@naver.com>
2021-06-18 22:55:26 +0900

    자동 스크린샷

--- GUI프로그래밍/5_listbox.py
+++ GUI프로그래밍/5_listbox.py
@@ -18,17 +18,17 @@ listbox.pack()
 
 def btncmd():
     # # 삭제
-    # listbox.delete(END) # 맨 뒤 항목을 삭제
-    # listbox.delete(0) # 맨 앞 항목을 삭제
+    listbox.delete(END) # 맨 뒤 항목을 삭제
+    listbox.delete(0) # 맨 앞 항목을 삭제
 
-    # # 갯수 확인
-    # print("리스트에는", listbox.size(), "개가 있어요")
+    # 갯수 확인
+    print("리스트에는", listbox.size(), "개가 있어요")
 
-    # # 항목 확인 (시작 idx, 끝 idx)
-    # print("1번째 부터 3번째 까지의 항목 : ", listbox.get(0,2)) # 0, 1, 2 인덱스 항목 출력
+    # 항목 확인 (시작 idx, 끝 idx)
+    print("1번째 부터 3번째 까지의 항목 : ", listbox.get(0,2)) # 0, 1, 2 인덱스 항목 출력
 
-    # 선택된 항목 확인 (위치로 반환 ex. (1, 2, 3))
-    print("선택된 항목 : ", listbox.curselection())
+    # # 선택된 항목 확인 (위치로 반환 ex. (1, 2, 3))
+    # print("선택된 항목 : ", listbox.curselection())
 
 btn = Button(root, text="클릭", command=btncmd)
 btn.pack()
